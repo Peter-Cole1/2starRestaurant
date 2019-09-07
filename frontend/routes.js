e0bf715cf669b9b553fad4c29cc3cb00e7cc48b9
fighterultimate <carlospsosame@gmail.com>
2019-09-07 13:24:28 -0400

    trying again

--- frontend/routes.js
+++ frontend/routes.js
@@ -4,10 +4,10 @@ var path = require("path");
 module.exports={
     displayTables:function(req,res)
     {
-        res.sendFile(path.join(__dirname, "tables.html"));
+        res.sendFile(path.join(__dirname, "/frontend/tables.html"));
     },
     displayReservationForm:function(req,res)
     {
-        res.sendFile(path.join(__dirname, "reserve.html"));
+        res.sendFile(path.join(__dirname, "/frontend/reserve.html"));
     }
 }
